a441dda3c5ae363ecd707c3f39574f2977621ef3
Victor <vik.sergienko@mail.ru>
2025-03-07 16:41:43 +0100

    Remove mutix

--- internal/db/engine/engine.go
+++ internal/db/engine/engine.go
@@ -1,9 +1,6 @@
 package engine
 
-import "sync"
-
 type Engine struct {
-	mutex     sync.RWMutex
 	hashTable map[string]string
 }
 
@@ -14,23 +11,14 @@ func New() *Engine {
 }
 
 func (e *Engine) Set(key string, value string) {
-	e.mutex.Lock()
-	defer e.mutex.Unlock()
-
 	e.hashTable[key] = value
 }
 
 func (e *Engine) Get(key string) (string, bool) {
-	e.mutex.RLock()
-	defer e.mutex.RUnlock()
-
 	data, ok := e.hashTable[key]
 	return data, ok
 }
 
 func (e *Engine) Del(key string) {
-	e.mutex.RLock()
-	defer e.mutex.RUnlock()
-
 	delete(e.hashTable, key)
 }
